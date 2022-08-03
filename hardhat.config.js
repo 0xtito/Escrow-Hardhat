e0ab6affcbe2495b77664583b31c0f1c7ca51cdf
tito <92818759+0xtito@users.noreply.github.com>
2022-08-03 17:27:20 -0400

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 module.exports = {
   solidity: "0.7.5",
   paths: {
     artifacts: "./app/artifacts",
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.INFURA_URL,
+      accounts: [process.env.INFURA_URL]
+    }
   }
 };
